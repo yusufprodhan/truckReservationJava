7d7f0e483f8006f9e2406344e31104252759af08
Md Yusuf Prodhan <yusuf.prodhan@shoplover.com>
2024-03-05 16:33:21 +0600

    init project

---
+++ src/main/java/com/truckreservation/truckreservation/TruckreservationApplication.java
@@ -0,0 +1,13 @@
+package com.truckreservation.truckreservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TruckreservationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TruckreservationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/truckreservation/truckreservation/TruckreservationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.truckreservation.truckreservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TruckreservationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
